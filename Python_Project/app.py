3670cd5761fcea6fa39d5711f73688920a34c99d
Hassan-Tuhin <hassantohin27@gmail.com>
2020-09-27 20:42:04 +0600

    27/09/2020

--- Python_Project/app.py
+++ Python_Project/app.py
@@ -1,17 +1,4 @@
-command=""
-while command.lower()!="quit":
-    command=input(">").lower()
-    if command=="start":
-        print("car started.")
-    elif command=="stop":
-        print("car stopped")
-    elif command=="help":
-        print("""
-        start:to start car
-        stop:to stop car
-        quit:to quit system
-        """)
-    elif command=="quit":
-        break
-    else:
-        print("can't understand command")
+cost=0
+for prices in [10,20,30]:
+    cost=cost+prices
+print(f'TOtal cost {cost}')
