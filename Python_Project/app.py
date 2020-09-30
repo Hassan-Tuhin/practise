e9e41911c8f34ef8fde8833a6107e6029cf622cc
Hassan-Tuhin <hassantohin27@gmail.com>
2020-10-01 00:09:14 +0600

    12:09 am

--- Python_Project/app.py
+++ Python_Project/app.py
@@ -1,19 +1,4 @@
-class Mammal():  #it's the parent class
-    def walk(self):
-        print("Walk")
-
-
-class Dog(Mammal):#We pass Mammel here as parent class.so all of the features of Mammal can be used by this class
-    def pet(self):
-        print("is a pet")
-
-
-class Cat(Mammal):
-    pass
-
-dog=Dog()
-dog.walk()#This is Mammal class method & we can ue it here by inheritance
-dog.pet()#This is the method of Dog class
-
-cat=Cat()
-cat.walk()
+from utils import find_max
+numbers=[2,3,4,1,6,7,5,8,10,11]
+max=find_max(numbers)
+print(f'largest number is:{max}')
