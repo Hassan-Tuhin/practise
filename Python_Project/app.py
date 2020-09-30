7592126e77637c197d39eba9cbe29788b0c6e511
Hassan-Tuhin <hassantohin27@gmail.com>
2020-09-30 10:32:08 +0600

    abc

--- Python_Project/app.py
+++ Python_Project/app.py
@@ -1,5 +1,14 @@
-try:
-    age=int(input('age: '))
-    print(age)
-except ValueError:
-    print("Invalid!!!!")
+class point():
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+    def move(self):
+        print("move")
+    def name(self):
+        print("pass")
+
+point2=point(10,20)
+print(point2.y)
+
+point2.y=25
+print(point2.y)
