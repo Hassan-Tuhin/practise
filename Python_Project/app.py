170be809db066739889d04c2dae2357cad09ea40
Hassan-Tuhin <hassantohin27@gmail.com>
2020-10-04 23:48:30 +0600

    text_file

--- Python_Project/app.py
+++ Python_Project/app.py
@@ -1,21 +1,16 @@
 class Profile():
+    num_of_employee=0
+    raise_amount=1.04
     def __init__(self,fname,lname,salary):
         self.fname=fname
         self.lname=lname
         self.salary=salary
 
+        Profile.num_of_employee+=1 #this will count number of employees
+
     def apply_raise(self):
-        self.salary=int(self.salary*1.04)
-
-shifuddin=Profile('Shifuddin','Tushar',450000)
+        self.salary=int(self.salary*Profile.raise_amount)
+print(f'Total employee:{Profile.num_of_employee}')
 tuhin=Profile('Hassan','Tuhin',500000)
+shifuddin=Profile('Shifuddin','Tushar',450000)
 
-print(f"tuhin's salry {tuhin.salary}")
-tuhin.apply_raise()
-print("tuhin's salary after applying apply_raise")
-print(tuhin.salary)
-
-print(f"shifuddin's salary {shifuddin.salary}")
-shifuddin.apply_raise()
-print("shifuddin's salary after applying apply_raise")
-print(shifuddin.salary)
