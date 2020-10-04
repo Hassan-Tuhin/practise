f6cb3757bc1580e124f4650c316ae778d4b10cc4
Hassan-Tuhin <hassantohin27@gmail.com>
2020-10-04 21:41:02 +0600

    practise class

--- Python_Project/app.py
+++ Python_Project/app.py
@@ -1,3 +1,21 @@
-from utils import Profile
-p1=Profile('Hassan','tuhin',20,500000)
-print(p1.age)
+class Profile():
+    def __init__(self,fname,lname,salary):
+        self.fname=fname
+        self.lname=lname
+        self.salary=salary
+
+    def apply_raise(self):
+        self.salary=int(self.salary*1.04)
+
+shifuddin=Profile('Shifuddin','Tushar',450000)
+tuhin=Profile('Hassan','Tuhin',500000)
+
+print(f"tuhin's salry {tuhin.salary}")
+tuhin.apply_raise()
+print("tuhin's salary after applying apply_raise")
+print(tuhin.salary)
+
+print(f"shifuddin's salary {shifuddin.salary}")
+shifuddin.apply_raise()
+print("shifuddin's salary after applying apply_raise")
+print(shifuddin.salary)
