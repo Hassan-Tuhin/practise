5a5a9dc3a52eea17583fbca76ee6db20877d83b3
Hassan-Tuhin <hassantohin27@gmail.com>
2020-10-04 10:37:54 +0600

    class practise

---
+++ Python_Project/app.py
@@ -0,0 +1,3 @@
+from utils import Profile
+p1=Profile('Hassan','tuhin',20,500000)
+print(p1.age)
--- Python_Project/utils.py
+++ Python_Project/utils.py
@@ -1,6 +1,18 @@
-def find_max(numbers):
+'''def find_max(numbers):
     maximum=numbers[0]
     for number in numbers:
         if number>maximum:
             maximum=number
-    return maximum
+    return maximum'''
+class Profile():
+    def __init__(self,fname,lname,age,salary):#it's a constructor
+        self.fname=fname
+        self.lname=lname
+        self.age=age
+        self.salary=salary
+        self.email=fname+lname+'@gmail.com'
+
+tuhin=Profile('Hassan','tuhin',20,500000)
+print(tuhin.email)
+tanbir=Profile('Tanbir','sakib',22,450000)
+print(tanbir.email)
