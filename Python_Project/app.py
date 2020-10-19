874d7d9257f462ca8b81183ff472282c5be79d47
Hassan-Tuhin <hassantohin27@gmail.com>
2020-10-19 21:30:07 +0600

    practise_dictionary

--- Python_Project/app.py
+++ Python_Project/app.py
@@ -1,34 +1,9 @@
-class Profile():
-    def __init__(self,fname,lname):
-        self.fname=fname
-        self.lname=lname
-        
-    @property
-    def email(self):
-        return f'{self.fname}{self.lname}@gmail.com'
-    @property
-    def fullname(self):
-        if self.fname==None or self.lname==None:
-            return'fullname not set.set it first'
-        return f'{self.fname} {self.lname}'
-    @fullname.setter
-    def fullname(self,name):
-        fname,lname=name.split(' ')
-        self.fname=fname
-        self.lname=lname
-    @fullname.deleter
-    def fullname(self):
-        self.fname=None
-        self.lname=None
-
-hassan=Profile('Hassan','Tuhin')
-hassan.fullname='G.H Tuhin'
-print(hassan.fullname)
-del hassan.fullname
-print(hassan.fullname)
-hassan.fullname='HHHH TTTT'
-print(hassan.fullname)
-
-
-#hassan.lname='Byt'
-#print(hassan.email)
+#d1={'Hassan':'meat','Tuhin':'Fish','Baizid':'Egg'}
+#print(d1['Baizid'])
+d2={'Hassan':'meat',
+    'Tuhin':'Fish',
+    'Fahim':'Egg',
+    'Tanbir':'Roti'
+    }
+for x,y in d2.items():
+    print(x,y)
