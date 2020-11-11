f5b7080715685b3786ad7510d4f1259f3c5c939e
Hassan-Tuhin <hassantohin27@gmail.com>
2020-11-11 19:11:49 +0600

    excel_update

--- Python_Project/app.py
+++ Python_Project/app.py
@@ -1,8 +1,3 @@
-import openpyxl as xl
-wb=xl.load_workbook("4A562000.xlsx")
-sheet=wb["Sheet2"]
-cell=sheet['C10']
-#a=sheet.cell(4,3)
-for row in range(4,11):
-    cell=sheet.cell(row,2)
-    print(cell.value)
+'''for row in range(4,11):
+    cell=sheet.cell(row,4)
+    print(cell.value)'''
